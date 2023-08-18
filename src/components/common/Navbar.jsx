3c5755ac233a39a0a56d4a44aa87aff259efd532
Luciano Angeleri <lcnang@gmail.com>
2023-08-18 15:35:30 -0300

    Se creó la estructura básica del navbar

---
+++ src/components/common/Navbar.jsx
@@ -0,0 +1,81 @@
+import { Fragment } from "react";
+import { CubeTransparentIcon, Bars3Icon, XMarkIcon, BellIcon } from "@heroicons/react/24/solid";
+import { Disclosure, Menu, Transition } from "@headlessui/react";
+
+const linksNavegacion = [
+    { name: "CV", href: "#seccionCV", current: true },
+    { name: "Proyectos", href: "#seccionProyectos", current: false },
+];
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join(" ");
+}
+const Navbar = () => {
+    return (
+        <header className="w-full fixed top-0 shadow-xl bg-primary text-light">
+            <Disclosure as="nav">
+                {({ open }) => (
+                    <>
+                        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+                            <div className="relative flex items-center justify-between">
+                                <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                                    <Disclosure.Button className="sm:hidden inline-flex items-center justify-center rounded-md p-2 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                                        {open ? (
+                                            <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                                        ) : (
+                                            <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                                        )}
+                                    </Disclosure.Button>
+                                </div>
+                                <div className="flex items-center justify-center m-auto md:m-0">
+                                    <div className="flex items-center gap-1 flex-col p-3">
+                                        <CubeTransparentIcon className="h-8 w-auto" />
+                                        <span className="font-display font-bold">Luciano Angeleri</span>
+                                    </div>
+                                    <div className="hidden my-auto sm:ml-6 sm:block">
+                                        <ul className="flex gap-3 items-center">
+                                            {linksNavegacion.map((item) => (
+                                                <li><a
+                                                    key={item.name}
+                                                    href={item.href}
+                                                    className={classNames(
+                                                        item.current
+                                                            ? "bg-gray-900 text-white"
+                                                            : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                                                        "rounded-md px-3 py-2 text-sm font-medium"
+                                                    )}
+                                                    aria-current={item.current ? "page" : undefined}>
+                                                    {item.name}
+                                                </a></li>
+                                            ))}
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <Disclosure.Panel className="sm:hidden">
+                            <div className="p-3 rounded-lg">
+                                {linksNavegacion.map((item) => (
+                                    <Disclosure.Button
+                                        key={item.name}
+                                        as="a"
+                                        href={item.href}
+                                        className={classNames(
+                                            item.current ? "border-2 text-light" : "hover:text-white",
+                                            "block rounded-lg px-3 py-2"
+                                        )}
+                                        aria-current={item.current ? "page" : undefined}>
+                                        {item.name}
+                                    </Disclosure.Button>
+                                ))}
+                            </div>
+                        </Disclosure.Panel>
+                    </>
+                )}
+            </Disclosure>
+        </header>
+    );
+};
+
+export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,36 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
+    colors: {
+      primary: "#698582",
+      accent:{
+        light: "#938D82",
+        dark: "#62513D",
+      },
+      dark: "#25262A",
+      light: "#EDF0EE",
+      white: "#FFFFFF",
+      black: "#000000",
+    },
+    fontFamily:{
+      display: '"Darker Grotesque", sans-serif',
+      heading: '"Encode Sans", sans-serif',
+    },
+    extend: {
   },
-  plugins: [],
+},
+plugins: [
+  require('@headlessui/tailwindcss')
+],
 }
 
