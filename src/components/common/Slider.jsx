31960a4628a81e2ceb541fc279873ee94276b432
Luciano Angeleri <lcnang@gmail.com>
2023-08-18 23:26:52 -0300

    Se corrigieron las rutas de las imagenes y se centro y dimensionó las imagenes de las tecnologias utilizadas

--- src/components/common/Slider.jsx
+++ src/components/common/Slider.jsx
@@ -6,19 +6,35 @@ import { Autoplay } from 'swiper/modules';
 const Slider = ({ tecnologias }) => {
     return (
         <Swiper
-            slidesPerView={2}
-            spaceBetween={30}
+            slidesPerView={1}
+            spaceBetween={32}
             loop={true}
+            centeredSlides={true}
+            
             autoplay={{
                 delay: 2500,
                 disableOnInteraction: false,
-              }}
+            }}
+            breakpoints={{
+                640: {
+                    slidesPerView: 1,
+                    spaceBetween: 32,
+                },
+                768: {
+                    slidesPerView: 3,
+                    spaceBetween: 32,
+                },
+                1024: {
+                    slidesPerView: 6,
+                    spaceBetween: 32,
+                },
+            }}
             modules={[Autoplay]}
             className="sliderTech"
-            >
+        >
             {tecnologias.map((tecnologia, index) => (
-                <SwiperSlide key={index}>
-                    <img src={tecnologia.src} alt={tecnologia.nombre} />
+                <SwiperSlide key={index} className='my-auto py-6'>
+                    <img src={tecnologia.src} alt={tecnologia.nombre} className='w-full drop-shadow-lg'/>
                 </SwiperSlide>
             ))}
         </Swiper>
--- src/components/views/SeccionTech.jsx
+++ src/components/views/SeccionTech.jsx
@@ -4,30 +4,32 @@ const SeccionTech = () => {
     const tecnologias = [
         {
             nombre: "Solidworks",
-            src: "../../assets/img/tecnologias/solidworks.svg",
+            src: "./src/assets/img/tecnologias/solidworks.svg",
         },
         {
             nombre: "Autodesk AutoCAD",
-            src: "/src/assets/img/tecnologias/autocad.png",
+            src: "./src/assets/img/tecnologias/autocad.png",
         },
         {
             nombre: "Autodesk",
-            src: "../../assets/img/tecnologias/autodesk.svg",
+            src: "./src/assets/img/tecnologias/autodesk.svg",
         },
         {
             nombre: "Utilmaker Cura",
-            src: "../../assets/img/tecnologias/ultimakerCura.png",
+            src: "./src/assets/img/tecnologias/ultimakerCura.png",
         },
         {
             nombre: "Artillery",
-            src: "../../assets/img/tecnologias/artillery.png",
+            src: "./src/assets/img/tecnologias/artillery.png",
         },
     ]
     return (
-        <section className='container bg-light' id="seccionTecnologias">
-            <h3 className="font-display font-black text-3xl text-dark mb-2">¿Qué tecnologías y herramientas utilizo?</h3>
-            <hr className="text-primary border-dashed mb-3" />
-            <Slider tecnologias={tecnologias} />
+        <section className='py-6 bg-light' id="seccionTecnologias">
+            <div className="container">
+                <h3 className="font-display font-black text-3xl text-dark mb-2">¿Qué tecnologías y herramientas utilizo?</h3>
+                <hr className="text-primary border-dashed mb-3" />
+                <Slider tecnologias={tecnologias}/>
+            </div>
         </section>
     );
 };
