3e661187b4fc0d0ad45b2b6637730b3418803acf
Luciano Angeleri <lcnang@gmail.com>
2023-08-20 13:56:01 -0300

    Se modificó el tamaño de los elementos del hero y se le agregó un fondo a la seccion

--- src/components/views/SeccionHero.jsx
+++ src/components/views/SeccionHero.jsx
@@ -5,17 +5,17 @@ import formaFlecha from "../../assets/img/svg/formaFlecha.svg"
 const SeccionHero = () => {
     return (
         <>
-            <section className="flex flex-col gap-8 pt-32 pb-12 bg-light">
+            <section className="flex flex-col gap-8 pt-32 pb-12 bg-light seccionHero">
                 <div className="container md:px-10 md:py-10">
                     <div className="flex items-center justify-evenly gap-4">
-                        <h1 className="text-center font-display font-black text-4xl text-primary leading-[85%] px-2 pt-3 pb-5">Luciano Angeleri</h1>
+                        <h1 className="text-center font-display font-black text-4xl md:text-7xl text-primary leading-[85%] md:leading-[85%] px-2 pt-3 pb-5">Luciano Angeleri</h1>
                         <div className="relative isolate">
-                            <img className="w-32 object-cover object-center rounded-b-3xl rounded-tr-3xl shadow-lg" alt="César Luciano Angeleri" src={imgLuciano} />
-                            <div className="absolute -top-5 -left-5" >
-                                <img alt="Centro" src={formaCentro} />
+                            <img className="w-32 md:w-64 lg:w-96 object-cover object-center rounded-b-3xl rounded-tr-3xl shadow-lg" alt="César Luciano Angeleri" src={imgLuciano} />
+                            <div className="absolute -top-5 -left-5 md:-top-12 md:-left-12" >
+                                <img className="md:w-20" alt="Centro" src={formaCentro} />
                             </div>
-                            <div className="absolute -bottom-4 -left-40" >
-                                <img className="" alt="Flecha" src={formaFlecha} />
+                            <div className="absolute -bottom-4 -left-40 md:-bottom-8 md:-left-64" >
+                                <img className="md:w-64" alt="Flecha" src={formaFlecha} />
                             </div>
                         </div>
                     </div>
